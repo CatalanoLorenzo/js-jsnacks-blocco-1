9c0445a9eb791b6aea385200516754dba7b89f23
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-14 11:44:22 +0100

    add if

--- jssnack 1/asset/js/main.js
+++ jssnack 1/asset/js/main.js
@@ -6,3 +6,11 @@ const numberone = Number(prompt ('inserisci un numero'))
 console.log(numberone)
 const numbertwo = Number(prompt ('inserisci un numero'))
 console.log(numbertwo)
+if(numberone > numbertwo){
+    console.log('il primo numero è il maggiore');
+}else if(numberone == numbertwo){
+    console.log('i due numero sono uguali')
+}
+else{
+    console.log('il secondo numero è maggiore');
+}
