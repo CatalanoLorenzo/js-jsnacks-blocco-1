948e31722cae4c890f9a6d1c7d9815f39d966181
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-14 11:45:58 +0100

    add coments

--- jssnack 1/asset/js/main.js
+++ jssnack 1/asset/js/main.js
@@ -2,15 +2,17 @@
 Il software stampa il maggiore. */
 
 /* L’utente inserisce due numeri in successione, */
-const numberone = Number(prompt ('inserisci un numero'))
-console.log(numberone)
-const numbertwo = Number(prompt ('inserisci un numero'))
-console.log(numbertwo)
-if(numberone > numbertwo){
+const numberone = Number(prompt('inserisci un numero'));
+console.log(numberone);
+const numbertwo = Number(prompt('inserisci un numero'));
+console.log(numbertwo);
+if (numberone > numbertwo) {
+    //stampa il maggiore.
     console.log('il primo numero è il maggiore');
-}else if(numberone == numbertwo){
-    console.log('i due numero sono uguali')
+} else if (numberone == numbertwo) {
+    console.log('i due numero sono uguali');
 }
-else{
+else {
+    //stampa il maggiore.
     console.log('il secondo numero è maggiore');
 }
