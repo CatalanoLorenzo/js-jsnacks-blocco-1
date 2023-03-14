d82a24de040ad4b668a0fd3f0c74f49f34b70342
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-14 11:41:51 +0100

    add frist step

---
+++ jssnack 1/asset/js/main.js
@@ -0,0 +1,8 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+/* L’utente inserisce due numeri in successione, */
+const numberone = Number(prompt ('inserisci un numero'))
+console.log(numberone)
+const numbertwo = Number(prompt ('inserisci un numero'))
+console.log(numbertwo)
