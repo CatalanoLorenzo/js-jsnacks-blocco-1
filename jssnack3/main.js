ac82ecef00be5d591317a4758bf9327b97f9d15d
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-15 11:48:55 +0100

    add cicle while

--- jssnack3/main.js
+++ jssnack3/main.js
@@ -1,6 +1,15 @@
-let numbertot = ''
-for(let i = 0; i< 10 ;i++){
+let numbertot = 0
+/* for(let i = 0; i< 10 ;i++){
     const number = (prompt('scrivi un numero'))
     numbertot += number 
 }
+console.log(numbertot)
+
+ */
+let i = 0
+while(i < 10){
+    const usernumber = Number(prompt('scrivi un numero'))
+    numbertot += usernumber
+    i++
+} 
 console.log(numbertot)
