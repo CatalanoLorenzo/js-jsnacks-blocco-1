2f369cb479e75f77e3bf13606ed30142eb31fefe
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-15 12:40:26 +0100

    second metod

--- jssnack5/main.js
+++ jssnack5/main.js
@@ -1,10 +1,24 @@
 const listaNumber =[
 
 ]
-for (let i = 0; i < 10; i++) {
+/* for (let i = 0; i < 10; i++) {
     let createNumber = Number(prompt('scegli un numero che non sia Fabio '))
     if(createNumber % 2 == 0){
         listaNumber.push (createNumber)
     }
+} */
+
+let i = 0
+while (i < 10) {
+    let createNumber = Number(prompt('scegli un numero che non sia Fabio '))
+    if(createNumber % 2 == 0){
+        listaNumber.push (createNumber)
+    }
+    i++
 }
+
+
+
+
+
 console.log(listaNumber);
